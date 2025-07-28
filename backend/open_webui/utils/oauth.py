77db3a33d7d95c31bc23523cd9f14b6067d7d397
Andrii Shyriaiev <a.shyriaiev@k2g.ai>
2025-07-28 03:51:01 +0300

    add logs

--- backend/open_webui/utils/oauth.py
+++ backend/open_webui/utils/oauth.py
@@ -350,7 +350,21 @@ class OAuthManager:
         if provider not in OAUTH_PROVIDERS:
             raise HTTPException(404)
         client = self.get_client(provider)
-        log.info(f'handle_callback provider: {provider}, request: {request}')
+        
+        req_json = {}
+        try:
+            req_json = request.json()
+        except Exception as e:
+            log.warning(f"handle_callback request.json() error: {e}")
+        
+        cookies = {}
+        try:
+            cookies = request.cookies
+        except Exception as e:
+            log.warning(f"handle_callback cookies error: {e}")
+
+        log.info(f'handle_callback provider: {provider}, req_json: {req_json}, cookies: {cookies}')
+        
         try:
             token = await client.authorize_access_token(request)
         except Exception as e:
