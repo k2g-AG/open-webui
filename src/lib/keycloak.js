785fee5ed3984c57f8df400f8e55c5695fc3f78e
Andrii Shyriaiev <a.shyriaiev@k2g.ai>
2025-07-30 04:58:06 +0300

    some fix for keycloak.js

--- src/lib/keycloak.js
+++ src/lib/keycloak.js
@@ -1,9 +1,10 @@
 import Keycloak from 'keycloak-js';
 
 const keycloak = new Keycloak({
-	url: 'https://kck-box-dev.k2g.ai',
+	url: 'https://kck-box-dev.k2g.ai/',
 	realm: 'openweb-ui',
-	clientId: 'openweb-ui'
+	clientId: 'openweb-ui',
+	onLoad: 'login-required'
 });
 
 export default keycloak;
