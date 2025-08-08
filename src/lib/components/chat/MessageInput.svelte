1821312ef2d2ebd9ffd52a7430ec751f64e5a6cc
Andrii Shyriaiev <a.shyriaiev@k2g.ai>
2025-08-08 07:19:28 +0300

    some filter fix

--- src/lib/components/chat/MessageInput.svelte
+++ src/lib/components/chat/MessageInput.svelte
@@ -665,11 +665,15 @@
 		if (directType) {
 			total_chatFiles.push(...inputFiles
 				// .filter((item) => ['file'].includes(item.type))
-				.filter((item) => ((item.file?.meta?.data?.uploadType === 'direct') && !(item.file?.meta?.data?.synthetic === true))));
+				// .filter((item) => ((item.file?.meta?.data?.uploadType === 'direct') && !(item.file?.meta?.data?.synthetic === true)))
+				.filter((item) => (!(item.file?.meta?.data?.synthetic === true)))
+			);
 		} else {
 			total_chatFiles.push(...inputFiles
 				// .filter((item) => ['file'].includes(item.type))
-				.filter((item) => (!(item.file?.meta?.data?.uploadType === 'direct') && !(item.file?.meta?.data?.synthetic === true))));
+				// .filter((item) => (!(item.file?.meta?.data?.uploadType === 'direct') && !(item.file?.meta?.data?.synthetic === true)))
+				.filter((item) => (!(item.file?.meta?.data?.synthetic === true)))
+			);
 		}
 		console.info('==> total_chatFiles:', {total_chatFiles});
 
