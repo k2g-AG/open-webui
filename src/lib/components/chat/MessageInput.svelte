948408762b9546a20ce714b63bbb9105c853134a
Andrii Shyriaiev <a.shyriaiev@k2g.ai>
2025-08-18 12:27:56 +0300

    add some logs

--- src/lib/components/chat/MessageInput.svelte
+++ src/lib/components/chat/MessageInput.svelte
@@ -1018,16 +1018,19 @@
 		if (e.dataTransfer?.files) {
 			const inputFiles = Array.from(e.dataTransfer?.files);
 			if (inputFiles && inputFiles.length > 0) {
-				console.info(inputFiles);
+				console.info('inputFiles:', inputFiles);
 				if ($user?.permissions?.chat?.file_direct_upload) {
 					console.info('drop files for direct upload...');
 					inputDirectFilesHandler(inputFiles);
 					filsDropProcessed = true;
 				} else {
+					console.info('no permission drop files for direct upload...');
 					if ($user?.permissions?.chat?.file_upload) {
 						console.info('drop files for vector upload...');
 						inputFilesHandler(inputFiles);
 						filsDropProcessed = true;
+					} else {
+						console.info('no permission drop files for vector upload...');
 					}
 				}
 			}
@@ -1036,6 +1039,7 @@
 		if (filsDropProcessed) {
 			return;
 		}
+		console.info('user permissions:', $user?.permissions);
 		toast.error($i18n.t('You do not have permission to upload files.'));
 	};
 
