9d94c26d44e7c6f9eb8641e23a57e407345e2d32
Andrii Shyriaiev <a.shyriaiev@k2g.ai>
2025-10-18 14:16:03 +0300

    fix logging

--- backend/open_webui/main.py
+++ backend/open_webui/main.py
@@ -489,9 +489,10 @@ try:
 except Exception as e:
     print(f"Google Cloud Logging not configured: {e}")
 
-logging.basicConfig(stream=sys.stdout, level=GLOBAL_LOG_LEVEL)
+# logging.basicConfig(stream=sys.stdout, level=GLOBAL_LOG_LEVEL)
 log = logging.getLogger(__name__)
-log.setLevel(SRC_LOG_LEVELS["MAIN"])
+# log.setLevel(SRC_LOG_LEVELS["MAIN"])
+log.setLevel(GLOBAL_LOG_LEVEL)
 
 
 class SPAStaticFiles(StaticFiles):
